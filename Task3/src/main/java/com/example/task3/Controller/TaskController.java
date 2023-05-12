8527928cca0a9bfcb5c61a9f237641c451366211
Kristina <kristi200483@gmail.com>
2023-05-12 13:28:37 +0300

    Add promethuse castom metrics

--- Task3/src/main/java/com/example/task3/Controller/TaskController.java
+++ Task3/src/main/java/com/example/task3/Controller/TaskController.java
@@ -1,6 +1,8 @@
 package com.example.task3.Controller;
 
 import com.example.task3.Configuration.TaskConfiguration;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,18 +14,21 @@ import java.util.Random;
 @RestController
 @Slf4j
 public class TaskController {
+    private final Counter tasks;
     private final RabbitTemplate rabbitTemplate;
     private final Random random;
 
     @Autowired
-    TaskController(RabbitTemplate rabbitTemplate) {
+    TaskController(RabbitTemplate rabbitTemplate, MeterRegistry meterRegistry) {
         this.rabbitTemplate = rabbitTemplate;
         this.random = new Random();
+        tasks = meterRegistry.counter("tasks_sender_total");
     }
 
     @PostMapping("/randomcreate")
     void randomcreate(Long number) {
         for (int i = 0; i < number;i++) {
+            tasks.increment();
             Long value = random.nextLong(1000);
             rabbitTemplate.convertAndSend(TaskConfiguration.QUEUE_NAME1, value);
         }
@@ -31,6 +36,7 @@ public class TaskController {
 
     @PostMapping("/createTask")
     void createTask(Long number) {
+        tasks.increment();
         rabbitTemplate.convertAndSend(TaskConfiguration.QUEUE_NAME1, number);
     }
 }
--- Task3Worker/src/main/java/com/example/task3worker/Service/WorkerService.java
+++ Task3Worker/src/main/java/com/example/task3worker/Service/WorkerService.java
@@ -1,6 +1,8 @@
 package com.example.task3worker.Service;
 
 import com.example.task3worker.Configuration.WorkerConfiguration;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -12,18 +14,33 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class WorkerService {
+    private final Counter answers;
+    private final Counter errors;
     private final RabbitTemplate rabbitTemplate;
 
-    public WorkerService (RabbitTemplate rabbitTemplate){
+    public WorkerService(RabbitTemplate rabbitTemplate, MeterRegistry meterRegistry) {
         this.rabbitTemplate = rabbitTemplate;
+        answers = meterRegistry.counter("answers_worker_total");
+        errors = meterRegistry.counter("errors_worker_total");
     }
 
     @RabbitListener(queues = {WorkerConfiguration.QUEUE_NAME1})
     public void work(Long number, @Header(AmqpHeaders.CONSUMER_QUEUE) String queueName, MessageHeaders headers) {
+        answers.increment();
         log.info("Got new message: " + number + ", headers: " + headers.values() + "queueName -" + queueName);
         try {
             Thread.sleep(number);
             rabbitTemplate.convertAndSend(WorkerConfiguration.QUEUE_NAME2, number);
+        } catch (Exception e) {
+            errors.increment();
+            log.info("Sleep Error");
+        }
+    }
+
+    public static void workFromController(Long number) {
+        log.info("Got new message from controller: " + number);
+        try {
+            Thread.sleep(number);
         } catch (Exception e) {
             log.info("Sleep Error");
         }
